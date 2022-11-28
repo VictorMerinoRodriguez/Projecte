14a58d8929d108f6a99046a419026d9377074e39
VictorMerinoRodriguez <91618488+VictorMerinoRodriguez@users.noreply.github.com>
2022-11-28 14:38:15 +0100

    Add files via upload

--- WEB/script.js
+++ WEB/script.js
@@ -1,30 +1,61 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-function createItem(){
-    const rouletteItem = document.createElement("i");
+function createItem(x){
+
+    const rouletteItem = document.createElement("div");
     rouletteItem.setAttribute("class","roulette-item");
-    x = Math.floor(Math.random()*37);
-    if(x == 0){
+
+    if(x==00){
         rouletteItem.style.backgroundColor = "var(--green-color)";
     }
-    else if(x >19){
-        rouletteItem.style.backgroundColor = "var(--black-color)";
-    }
-    else {
+    else if(x%2==0){
         rouletteItem.style.backgroundColor = "var(--red-color)";
     }
+    else{
+        rouletteItem.style.backgroundColor = "var(--black-color)";
+    }
+    var number=document.createTextNode(x)
+    rouletteItem.appendChild(number);
     return rouletteItem
 
 }
 
+function createNumber(n){
+    var x=[];
+    var y=[];
+    for(var i=0;i<n;i++){
+        if(i%2==0){
+            x.push(i);
+        }else{
+            y.push(i);
+        }
+    }
+    return [x,y]
+}
+
 async function spin(){
-    for(var i = 0; i < 5; i++){
-        for(var i = 0; i < 9; i++){
-            await sleep(100*i);
-            var rouletteItem = createItem();
+    let numberArray=createNumber(36);
+    let evenNumbers=numberArray[0];
+    let oddNumbers=numberArray[1];
+
+        for(var i =0;i<45;i++){
+            if(i%36==0){
+                var evenNumbersClone = [...evenNumbers];
+                var oddNumbersClone = [...oddNumbers];
+            }
+
+            if(i%2==0){
+                var x=evenNumbersClone
+            }
+            else{
+                var x=oddNumbersClone
+            }
+            var RandomInt=Math.floor(Math.random()*x.length);
+            await sleep(10*i);
+            var rouletteItem = createItem(x[RandomInt]);
+            x.splice(RandomInt,1);
             const roulette = document.getElementById('roulette');
             roulette.appendChild(rouletteItem);
+            roulette.firstChild.remove();
         }
-    document.getElementsByClassName("roulette-item").remove();
-}
 }
